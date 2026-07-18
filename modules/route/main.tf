11318da99a3e9aedd59b89d2e9157e3455ce6b6f
app-yevgeni <23049337+yevgeni-max2006@users.noreply.github.com>
2026-07-18 19:07:47 +0300

    Update main.tf

--- modules/route/main.tf
+++ modules/route/main.tf
@@ -1,10 +1,27 @@
 
+resource "kubernetes_secret" "minio_console_tls" {
+  metadata {
+    name      = "minio-console-tls"
+    namespace = "default"
+  }
+
+  type = "kubernetes.io/tls"
+
+  data = {
+    "tls.crt" = file("${path.module}/tls.crt")
+    "tls.key" = file("${path.module}/tls.key")
+  }
+}
+
 resource "kubernetes_manifest" "minio_gateway" {
+  depends_on = [
+    kubernetes_secret.minio_console_tls
+  ]
   manifest = {
     apiVersion = "gateway.networking.k8s.io/v1"
     kind       = "Gateway"
     metadata = {
-      name      = "minio-console"
+      name      = "minio"
       namespace = "default"
     }
 
